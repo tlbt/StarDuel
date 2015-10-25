4a03663968cfe6bd0a4c7558917f807e7bc91740
Bruce <bruce@bruce-Latitude-D610.(none)>
2015-10-25 13:53:36 -0700

    Added a second sprite, actor array, update for loops, Cruise constructor and player keyboard setup function.

--- keyboard.js
+++ keyboard.js
@@ -49,3 +49,35 @@ function checkRotation(someShip){
 	someShip.model.vy = Math.sin(someShip.model.rotation) * someShip.speed;
 	someShip.model.vx = Math.cos(someShip.model.rotation) * someShip.speed;
 }
+
+function setupPlayer1Kb(someShip){
+		
+	 //Left arrow key `press` method
+  left.press = function() {
+	console.log("left pressed"); 
+    //Change the spaceShip's velocity when the key is pressed
+    someShip.model.rotation -= someShip.turnRate;
+    
+  };
+
+  //Up
+  up.press = function() {
+    console.log("up pressed"); 
+    someShip.speed == someShip.maxSpeed ? (someShip.speed = someShip.maxSpeed) : (someShip.speed += someShip.speedInc);
+    someShip.model.vy += Math.sin(someShip.model.rotation) * someShip.speed;
+	someShip.model.vx += Math.cos(someShip.model.rotation) * someShip.speed;
+  };
+
+  //Right
+  right.press = function() {
+	  console.log("right pressed"); 
+    someShip.model.rotation += someShip.turnRate;
+  };
+
+  //Down
+  down.press = function() {
+		console.log("down pressed");
+		someShip.speed == 0 ? (someShip.speed = 0) : (someShip.speed -= someShip.speedInc);
+		someShip.model.vy -= Math.sin(someShip.model.rotation) * someShip.speed;
+		someShip.model.vx -= Math.cos(someShip.model.rotation) * someShip.speed;
+  };}
